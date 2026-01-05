b9a1c386f506c3b45ead18279516d7c43e60f613
swbianchi <98054093+swbianchi@users.noreply.github.com>
2026-01-05 14:46:31 -0800

    Add files via upload

---
+++ ps0/tests/q2_3_1.py
@@ -0,0 +1,10 @@
+# +
+OK_FORMAT = True
+
+test = {   'name': 'q2_3_1',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> seconds_in_a_decade == 60 * 60 * 24 * 365 * 8 + 60 * 60 * 24 * 366 * 2\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ ps0/tests/q4_4_1.py
@@ -0,0 +1,5 @@
+OK_FORMAT = True
+
+test = {   'name': 'q4_4_1',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> gdp.shape == (12376, 52)\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
